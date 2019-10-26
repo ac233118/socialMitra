25db011bdbb8830a801d893b078763076842a5ed
Akshay Chavan <ac233118@gmail.com>
2019-10-26 17:43:33 +0530

    Changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = new express();
 
 app.use(express.static(__dirname + '/images'))
+app.use(express.static(__dirname + '/style'))
 
 app.get('/' ,(req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
