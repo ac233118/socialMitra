df3c448e2019069e940116688f3bbb0e6d08778f
Akshay Chavan <ac233118@gmail.com>
2019-10-26 13:15:25 +0530

    first

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = new express();
+
+app.use(express.static(__dirname + '/images'))
+
+app.get('/' ,(req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(process.env.PORT || 8080)
